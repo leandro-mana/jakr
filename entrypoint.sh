f19af8c10840f95f387c81dbe35e48d56eb63ad8
Leandro Mana <leandromana@gmail.com>
2022-05-11 19:19:56 +1200

    Update entrypoint - API body

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,7 +80,7 @@ function set_github_release {
             # echo ${BODY} | jq
 
             # GitHub Release API
-            curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${API_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/releases -d \'\{"tag_name":\"v${VERSION}\","target_commitish":\"${MASTER}\","name":\"v${VERSION}\","body":\"${KEYWORD}\","draft":false,"prerelease":false\}\' | jq .
+            curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${API_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/releases -d \'\{\"tag_name\":\"v${VERSION}\",\"target_commitish\":\"${MASTER}\",\"name\":\"v${VERSION}\",\"body\":\"${KEYWORD}\",\"draft\":false,\"prerelease\":false\}\' | jq .
             exit 0
         fi
 
