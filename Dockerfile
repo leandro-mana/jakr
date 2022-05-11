e543f92aa8f05450cd3923b5d6d278fc20175995
Leandro Mana <leandromana@gmail.com>
2022-05-11 18:32:57 +1200

    Add initial set of full functionalities definition

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM alpine
+
+LABEL "maintainer"="leandro-mana"
+LABEL "repository"="https://github.com/leandro-mana/jakr"
+LABEL "com.github.actions.name"="jakr"
+LABEL "com.github.actions.description"="Create a Release based on commit message keyword"
+LABEL "com.github.actions.icon"="gift"
+LABEL "com.github.actions.color"="green"
+
+RUN apk add --no-cache \
+        bash \
+        curl \
+        git \
+        jq && \
+        which bash && \
+        which curl && \
+        which jq && \
+        which git
+
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
+COPY sample_push_event.json /sample_push_event.json
+
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+# This Script acts as the Entrypoint for the Docker Image, providing the needed
+# Functionality for the GitHub Actions - JAKR: Just Another Keyword Releaser - 
+# to work as expected, by creating a Release based on a Git Commit Message Keyword
+# Notes:
+# - Env Vars like GITHUB_* are in the GitHub Actions Working Environment
+
+# ERREXIT - Exit at first Script Error
+set -e
+
+# Git Message containing the keyword
+KEYWORD=$*
+RELEASER='JARK'
+
+# Functions definition
+function log_message {
+    # This Function act as logger for better finding of messages in the output
+    # IMPUT as String Message (i.e. log_message "this message")
+    MESSAGE=${1}
+    NUM_OF_CHARS=$(echo ${MESSAGE} | wc -c)
+    NUM_OF_CHARS=$((${NUM_OF_CHARS} + 3))
+    LINE_OF_CHARS=$(printf %${NUM_OF_CHARS}s | tr " " '#')
+    echo ${LINE_OF_CHARS}
+    echo "# ${MESSAGE} #"
+    echo ${LINE_OF_CHARS}
+
+}
+
+
+function get_event_path {
+    # This Function will define the Event to process
+    # by Checking if running on a GitHub Actions, otherwise use Sample Event
+    if [ "${GITHUB_EVENT_PATH}" ]; then
+        EVENT_PATH=$GITHUB_EVENT_PATH
+
+    elif [ -f ./sample_push_event.json ]; then
+        EVENT_PATH='./sample_push_event.json'
+        LOCAL_TEST='true'
+
+    else
+
+        log_message "JSON Event, Not Found."
+        exit 1
+    fi
+
+    log_message "Processing EVENT_PATH: ${EVENT_PATH}"
+}
+
+
+function get_environment {
+    # This Function will display:
+    # - Environment Variables
+    # - JSON Event
+    log_message "Logging Environment"
+    env
+    jq . < $EVENT_PATH
+}
+
+
+function set_github_release {
+    # This Function will check the exact supported KEYWORD to process the GitHub Release
+    # based on the branch name and current date for version
+    TRUE=$(jq '.commits[].message, .head_commit.message' < ${EVENT_PATH} | grep -w "${KEYWORD}")
+    if [ "${TRUE}" ]; then
+        if [ "${LOCAL_TEST}" ]; then
+            log_message "[TESTING] - KEYWORD:${KEYWORD} - was found, no GitHub Release created."
+            exit 0
+
+        else
+            # Get Branch and Version
+            # NOTE: git global setting needed to run in GitHub Workflow Environment
+            git config --global --add safe.directory /github/workspace
+            MASTER=$(git rev-parse --abbrev-ref HEAD)
+            VERSION=$(date +%F.%s)
+
+            # Set DATA Body for GitHub Release API            
+            BODY="{\"tag_name\":\"v${VERSION}\",\"target_commitish\":\"${MASTER}\",\"name\":\"v${VERSION}\",\"body\":\"${KEYWORD}\",\"draft\":false,\"prerelease\":false}"
+
+            log_message "POST data for GitHub Release API"
+            echo ${BODY} | jq
+
+            # GitHub Release API
+            REQUEST=$(curl -X POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${API_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/releases -d \'${BODY}\' && exit 1)
+            echo ${REQUEST} | jq .
+        fi
+
+    else
+        # exit gracefully
+        log_message "KEYWORD: ${KEYWORD} - Not Found, Nothing to process."
+        exit 0
+
+    fi
+
+}
+
+# Run
+log_message "GitHub Keyboard Releaser:${RELEASER} Starting Execution"
+get_event_path
+get_environment
+set_github_release
